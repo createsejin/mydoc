11a433060b1ad4c17723a72aeeb50218366abf11
createjin <createsejin@gmail.com>
2024-03-27 00:08:18 +0900

    2024-03-26-002

---
+++ scripts/nvim_backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+shopt -s dotglob
+if [ "$1" = "restore2" ]; then
+  cd /home/bae/.config/nvim.test2
+  rm -rf *
+  cd ..
+  cp -r nvim.test/* nvim.test2
+fi
+if [ "$1" = "copy" ]; then
+  # delete previous files
+  cd /home/bae/.config/nvim.bak
+  mkdir delete
+  mv * delete
+  # copy original files
+  cd /home/bae/.config/nvim
+  cp -r * /home/bae/.config/nvim.bak
+  cd /home/bae/.config/nvim.bak
+  rm -rf .git .gitignore
+  # move files to keep
+  cd delete
+  mv .git .gitignore my_old_init ../
+  cd ..
+  rm -rf delete
+fi
+if [ "$1" = "zip" ]; then
+  cd /home/bae/.config/nvim.test2
+  bsdtar --acls --xattrs -cpaf - nvchat_git | pv -s $(\du -sb . | awk '{print $1}') \
+  | pbzip2 > "nvchat_git.tar.gz"
+fi
+if [ "$1" = "unzip" ]; then
+  cd /home/bae/.config/nvim.test2
+  pv "nvchat_git.tar.gz" | pbzip2 -dc | bsdtar --acls --xattrs -xpzf -
+fi
