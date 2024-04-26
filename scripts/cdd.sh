d9d6cb370a60f320b24da6086a2693e1ec65487a
createjin <createsejin@gmail.com>
2024-04-26 18:49:06 +0900

    fix cdd.sh: copy cd commands to system cilpboard(wl-clipboard)

--- scripts/cdd.sh
+++ scripts/cdd.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
-if [ "$1" = "vim-st" ]
-  cd /home/bae/.local/state/nvim
+
+check_file_exist() {
+  if [ -f "$1" ]; then
+    return 1
+  else
+    return 0
+  fi
+}
+main_com_zsh="/home/bae/Documents/configs/.zshrc"
+check_file_exist "$main_com_zsh"
+check_main=$?
+if [ "$check_main" -eq 1 ]; then
+  home="/home/bae"
+else
+  home="/mnt/root/home/bae"
+fi
+
+if [ "$1" = "vim-st" ]; then
+  location="$home/.local/state/nvim"
+  cd "$location"
+  ls -Al
+  echo "cd $location" | wl-copy
+fi
+if [ "$1" = "vim-swap" ]; then
+  location="$home/.local/state/nvim/swap"
+  cd $location
+  ls -Al
+  echo "cd $location" | wl-copy
+fi
+if [ "$1" = "vim-view" ]; then
+  location="$home/.local/state/nvim/view"
+  cd $location
+  ls -Al
+  echo "cd $location" | wl-copy
 fi
