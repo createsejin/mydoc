474012baff9a7be95b8c2ae0ce5d31ce28fbf0e5
createjin <createsejin@gmail.com>
2024-05-01 15:45:49 +0900

    cdd.sh: add help message

--- scripts/cdd.sh
+++ scripts/cdd.sh
@@ -22,22 +22,44 @@ cdd_func() {
   ls -Al --color=auto
   echo "cd $location" | wl-copy
 }
+# variables
+vim_st="$home/.local/state/nvim"
+vim_swap="$home/.local/state/nvim/swap"
+vim_view="$home/.local/state/nvim/view"
+se="$home/Documents/sessions"
+doc="$home/Documents"
+script="$home/Documents/scripts/.scripts"
+obsidian="$home/Obsidian"
+
+if [ "$1" = "help" ]; then
+  echo "vim-st: $vim_st"
+  echo "vim-swap: $vim_swap"
+  echo "vim-view: $vim_view"
+  echo "se: $se"
+  echo "d: $doc"
+  echo "sc: $script"
+  echo "ob: $obsidian"
+  echo "to move dir, press Ctrl+p in tmux"
+fi
 
 if [ "$1" = "vim-st" ]; then
-  cdd_func "$home/.local/state/nvim"
+  cdd_func $vim_st
 fi
 if [ "$1" = "vim-swap" ]; then
-  cdd_func "$home/.local/state/nvim/swap"
+  cdd_func $vim_swap
 fi
 if [ "$1" = "vim-view" ]; then
-  cdd_func "$home/.local/state/nvim/view"
+  cdd_func $vim_view
 fi
 if [ "$1" = "se" ]; then
-  cdd_func "$home/Documents/sessions"
+  cdd_func $se
 fi
 if [ "$1" = "d" ]; then
-  cdd_func "$home/Documents"
+  cdd_func $doc
 fi
 if [ "$1" = "sc" ]; then
-  cdd_func "$home/Documents/scripts/.scripts"
+  cdd_func $script
+fi
+if [ "$1" = "ob" ]; then
+  cdd_func $obsidian
 fi
