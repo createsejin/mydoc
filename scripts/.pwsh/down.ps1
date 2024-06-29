908d7e71477aca282f527e705cd28f69fda749a5
createjin <createsejin@gmail.com>
2024-06-29 16:27:39 +0900

    make windows config file copy script: copy to origin config file path from mydoc config files

--- scripts/.pwsh/down.ps1
+++ scripts/.pwsh/down.ps1
@@ -1,6 +1,6 @@
 $commander = "C:\Users\creat\Projects\the_cave\tests\downloader_test02\Commander\bin\Debug\net8.0\Commander.exe"
 $controller = "C:\Users\creat\Projects\the_cave\tests\downloader_test02\Controller\bin\Debug\net8.0\Controller.exe"
-$searcher = "C:\Users\creat\Projects\the_cave\tests\downloader_test02\Searcher\bin\Debug\net8.0"
+$searcher = "C:\Users\creat\Projects\the_cave\tests\downloader_test02\Searcher\bin\Debug\net8.0\Searcher.exe"
 
 if ($args[0] -eq "cmd") {
   Start-Process -FilePath $commander
