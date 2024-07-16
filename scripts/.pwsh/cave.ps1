f74eae306697e15d9d0a974f6b729d7a5455b68a
createjin <createsejin@gmail.com>
2024-07-17 01:22:11 +0900

    window terminal scroll key binds set and some slight changes of settings

--- scripts/.pwsh/cave.ps1
+++ scripts/.pwsh/cave.ps1
@@ -3,13 +3,13 @@ $commander = "$commander_path\Commander.exe"
 $downloader_path = "C:\Users\creat\Projects\the_cave\downloader\Operator\bin\Debug\net8.0"
 $downloader = "$downloader_path\Operator.exe"
 
-if ($args[0] -eq "cmd") {
+if ($args[0] -ieq "cmd") {
   Start-Process -FilePath "pwsh" -Wait -ArgumentList "-NoExit", "-c", "$commander"
 }
-elseif ($args[0] -eq "main") {
+elseif ($args[0] -ieq "main") {
   Start-Process -FilePath "pwsh" -Wait -ArgumentList "-NoExit", "-c", "$downloader"
 }
-elseif ($args[0] -eq "t001") {
+elseif ($args[0] -ieq "t001") {
   wt --maximized -p "PowerShell" -d "$downloader_path" pwsh -NoExit -c "$downloader" `; `
     sp -p "PowerShell" -V -s .67 -d "$commander_path" pwsh -NoExit -c "$commander" `; `
     sp -p "PowerShell" -V -s .5 -d "$home" `; `
