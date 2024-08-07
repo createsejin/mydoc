fbefe038f662c6f62b025243d1240fd888822971
createjin <createsejin@gmail.com>
2024-08-07 21:52:02 +0900

    2024-08-07-01

--- scripts/.pwsh/cave.ps1
+++ scripts/.pwsh/cave.ps1
@@ -2,8 +2,8 @@ $commander_path = "C:\Users\creat\Projects\the_cave\downloader\Commander\bin\Deb
 $commander = "$commander_path\Commander.exe"
 $operator_path = "C:\Users\creat\Projects\the_cave\downloader\Operator\bin\Debug\net8.0"
 $operator = "$operator_path\Operator.exe"
-$downloader_path = "C:\Users\creat\Projects\the_cave\downloader\Downloader\bin\Debug\net8.0"
-$downloader = "$downloader_path\Downloader.exe"
+# $downloader_path = "C:\Users\creat\Projects\the_cave\downloader\Downloader\bin\Debug\net8.0"
+# $downloader = "$downloader_path\Downloader.exe"
 
 if ($args[0] -ieq "cmd") {
   Start-Process -FilePath "pwsh" -Wait -ArgumentList "-NoExit", "-c", "$commander"
@@ -20,9 +20,7 @@ elseif ($args[0] -ieq "t001") {
 elseif ($args[0] -ieq "-h") {
   wt --pos 5360,0 --maximized --title "TheCave" -p "PowerShell" `
     -d "$operator_path" pwsh -NoExit -c "$operator" --headless `; `
-    sp -V -p "PowerShell" --title "TheCave" `
-    -d "$downloader_path" pwsh -NoExit -c "$downloader" `; `
-    sp -H --size .54 -p "PowerShell" --title "TheCave" -d "$operator_path" `
+    sp -V --size .44 -p "PowerShell" --title "TheCave" -d "$operator_path" `
     pwsh -NoExit -c Write-Output "execute unzipper" `; `
     mf left `; sp -H --size .13 --title "TheCave" -d "$commander_path" `
     pwsh -NoExit -c "$commander"
@@ -30,12 +28,10 @@ elseif ($args[0] -ieq "-h") {
 else {
   wt --pos 5360,0 --maximized --title "TheCave" -p "PowerShell" `
     -d "$operator_path" pwsh -NoExit -c "$operator" `; `
-    sp -V -p "PowerShell" --title "TheCave" `
-    -d "$downloader_path" pwsh -NoExit -c "$downloader" `; `
-    sp -H --size .54 -p "PowerShell" --title "TheCave" -d "$operator_path" `
+    sp -V --size .44 -p "PowerShell" --title "TheCave" -d "$operator_path" `
     pwsh -NoExit -c Write-Output "execute unzipper" `; `
     mf left `; sp -H --size .13 --title "TheCave" -d "$commander_path" `
     pwsh -NoExit -c "$commander"
   <#
-  cave @#down#>
+  cave @#down #>
 }
